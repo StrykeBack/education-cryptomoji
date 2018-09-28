b3e5ee9bc780c586c49e4d53d29db0da753874ce
Dan Guan <daniel.guan.bca@gmail.com>
2018-09-28 18:19:06 -0400

    Finish createCollection
    - Create new moji with pseudorandom dna
      - Encode these mojis and add to context
    - Add new collection to context linking to new mojis

--- code/part-two/processor/actions/createCollection.js
+++ code/part-two/processor/actions/createCollection.js
@@ -1,9 +1,13 @@
 const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
 
 const { encode } = require('../services/encoding');
-const { getCollectionAddress } = require('../services/addressing');
+const {
+  getCollectionAddress,
+  getMojiAddress
+} = require('../services/addressing');
+const getPrng = require('../services/prng');
 
-let createCollection = (res, rej, txn, context) => {
+const createCollection = (res, rej, txn, context) => {
   let signer = getCollectionAddress(txn.header.signerPublicKey);
   context.getState([signer]).then(allAddrs => {
     if (allAddrs[signer].length > 0) {
@@ -11,13 +15,41 @@ let createCollection = (res, rej, txn, context) => {
     } else {
       let updates = {};
       let mojiArr = [
-        txn.signature.substring(0, 70),
-        txn.signature.substring(0, 70),
-        txn.signature.substring(0, 70)
+        {
+          addr: getMojiAddress(
+            txn.header.signerPublicKey,
+            createDna('zero', txn.signature)
+          ),
+          dna: createDna('zero', txn.signature)
+        },
+        {
+          addr: getMojiAddress(
+            txn.header.signerPublicKey,
+            createDna('one', txn.signature)
+          ),
+          dna: createDna('one', txn.signature)
+        },
+        {
+          addr: getMojiAddress(
+            txn.header.signerPublicKey,
+            createDna('two', txn.signature)
+          ),
+          dna: createDna('two', txn.signature)
+        }
       ];
+      for (let i = 0; i < mojiArr.length; i++) {
+        updates[mojiArr[i].addr] = encode({
+          dna: mojiArr[i].dna,
+          owner: txn.header.signerPublicKey,
+          breeder: '',
+          sire: '',
+          bred: [],
+          sired: []
+        });
+      }
       updates[signer] = encode({
         key: txn.header.signerPublicKey,
-        moji: mojiArr
+        moji: mojiArr.map(moji => moji.addr)
       });
       context
         .setState(updates)
@@ -29,6 +61,16 @@ let createCollection = (res, rej, txn, context) => {
         });
     }
   });
+};
+
+const createDna = (randStr, signature) => {
+  let dnaStr = '';
+  let randomNum =
+    getPrng(randStr.toString(16))(9999999) + getPrng(signature)(9999999);
+  while (dnaStr.length < 36) {
+    dnaStr += randomNum.toString(16);
+  }
+  return dnaStr.substring(0, 36);
 };
 
 module.exports = {
--- code/part-two/processor/tests/03-CreateCollection.js
+++ code/part-two/processor/tests/03-CreateCollection.js
@@ -12,7 +12,6 @@ const { decode } = require('./services/encoding');
 const Txn = require('./services/mock_txn');
 const Context = require('./services/mock_context');
 
-
 describe('Create Collection', function() {
   let handler = null;
   let context = null;
@@ -32,42 +31,41 @@ describe('Create Collection', function() {
   });
 
   it('should create collections at correct addresses', function() {
-    return handler.apply(txn, context)
-      .then(() => {
-        expect(context._state[address]).to.exist;
+    return handler.apply(txn, context).then(() => {
+      expect(context._state[address]).to.exist;
 
-        const collection = decode(context._state[address]);
-        expect(collection.key).to.equal(publicKey);
-        expect(collection.moji).to.be.an('array');
-      });
+      const collection = decode(context._state[address]);
+      expect(collection.key).to.equal(publicKey);
+      expect(collection.moji).to.be.an('array');
+    });
   });
 
   it('should create three moji for each new collection', function() {
-    return handler.apply(txn, context)
-      .then(() => {
-        const collection = decode(context._state[address]);
-        const mojiAddress = collection.moji[0];
+    return handler.apply(txn, context).then(() => {
+      const collection = decode(context._state[address]);
+      const mojiAddress = collection.moji[0];
 
-        expect(collection.moji).to.have.lengthOf(3);
-        expect(mojiAddress).to.be.a.hexString.with.lengthOf(70);
-        expect(context._state[mojiAddress]).to.exist;
+      expect(collection.moji).to.have.lengthOf(3);
+      expect(mojiAddress).to.be.a.hexString.with.lengthOf(70);
+      expect(context._state[mojiAddress]).to.exist;
 
-        const moji = decode(context._state[mojiAddress]);
-        expect(moji.dna).to.be.a.hexString.with.lengthOf(36);
-        expect(mojiAddress).to.equal(getMojiAddress(publicKey, moji.dna));
-      });
+      const moji = decode(context._state[mojiAddress]);
+      expect(moji.dna).to.be.a.hexString.with.lengthOf(36);
+      expect(mojiAddress).to.equal(getMojiAddress(publicKey, moji.dna));
+    });
   });
 
   it('should create moji deterministically', function() {
     let oldMoji = null;
 
-    return handler.apply(txn, context)
+    return handler
+      .apply(txn, context)
       .then(() => {
         const collection = decode(context._state[address]);
         oldMoji = collection.moji;
 
         // Delete the created collection and cryptomoji
-        oldMoji.concat(address).forEach(addr => delete context._state[addr] );
+        oldMoji.concat(address).forEach(addr => delete context._state[addr]);
 
         return handler.apply(txn, context);
       })
@@ -80,18 +78,20 @@ describe('Create Collection', function() {
   it('should create moji pseudorandomly', function() {
     let oldMoji = null;
 
-    return handler.apply(txn, context)
+    return handler
+      .apply(txn, context)
       .then(() => {
         const collection = decode(context._state[address]);
         oldMoji = collection.moji;
 
         // Delete the created collection and cryptomoji
-        oldMoji.concat(address).forEach(addr => delete context._state[addr] );
+        oldMoji.concat(address).forEach(addr => delete context._state[addr]);
 
         // Modify a character in the signature to change the prng seed
-        const firstSig = txn.signature[0] !== 'f'
-          ? (parseInt(txn.signature[0], 16) + 1).toString(16)
-          : '0';
+        const firstSig =
+          txn.signature[0] !== 'f'
+            ? (parseInt(txn.signature[0], 16) + 1).toString(16)
+            : '0';
         txn.signature = firstSig + txn.signature.slice(1);
 
         return handler.apply(txn, context);
@@ -103,7 +103,8 @@ describe('Create Collection', function() {
   });
 
   it('should reject a public key that has already been used', function() {
-    const submission = handler.apply(txn, context)
+    const submission = handler
+      .apply(txn, context)
       .then(() => handler.apply(txn, context));
 
     return expect(submission).to.be.rejectedWith(InvalidTransaction);
